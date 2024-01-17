0a81cb534d8a0a919c77236870d5e377932d5fdc
yuanzhongshang <37822515+yuanzhongshang@users.noreply.github.com>
2024-01-17 08:34:03 -0500

    Add files via upload

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -5,7 +5,15 @@ GIFT_individualcpp <- function(X, y, Zx, Zy, Omega, constralpha, constrp, maxIte
     .Call(`_GIFT_GIFT_individualcpp`, X, y, Zx, Zy, Omega, constralpha, constrp, maxIter, epsStopLogLik)
 }
 
+GIFT_individualcpppleio <- function(X, y, Zx, Zy, Omega, constralpha, constrp, maxIter, epsStopLogLik, pleioindex) {
+    .Call(`_GIFT_GIFT_individualcpppleio`, X, y, Zx, Zy, Omega, constralpha, constrp, maxIter, epsStopLogLik, pleioindex)
+}
+
 GIFT_summarycpp <- function(betax, betay, Sigma1s, Sigma2s, Omega, constralpha, constrp, k, n1, n2, maxIter, epsStopLogLik) {
     .Call(`_GIFT_GIFT_summarycpp`, betax, betay, Sigma1s, Sigma2s, Omega, constralpha, constrp, k, n1, n2, maxIter, epsStopLogLik)
+}
+
+GIFT_summarycpppleio <- function(betax, betay, Sigma1s, Sigma2s, Omega, constralpha, constrp, k, n1, n2, maxIter, epsStopLogLik, pleioindex) {
+    .Call(`_GIFT_GIFT_summarycpppleio`, betax, betay, Sigma1s, Sigma2s, Omega, constralpha, constrp, k, n1, n2, maxIter, epsStopLogLik, pleioindex)
 }
 
